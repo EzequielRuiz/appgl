1c8ae12f0677a25de4ca783400cf2f431b2dea01
Ezequiel Ruiz <ruizvillamaria@gmail.com>
2023-08-08 09:22:25 -0300

    commit

--- components/SidebarPlugin/SideBar.vue
+++ components/SidebarPlugin/SideBar.vue
@@ -3,7 +3,7 @@
     <div class="sidebar-wrapper" ref="sidebarScrollArea">
       <div class="logo">
         <a href="https://miemmtec.com.ar" class="simple-text logo-mini">
-          <img src="img/placeholder.jpg" alt="app-logo" />
+          <img src="static/logo_whitepng.png" alt="app-logo" />
         </a>
         <a href="https://miemmtec.com.ar" class="simple-text logo-normal">
           {{ title }}
